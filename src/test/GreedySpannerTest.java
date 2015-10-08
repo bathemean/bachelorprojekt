1d23433c10ca7156ee4d3eeb0b105e6a7f5f373a
Allan Nielsen <an@quantads.net>
2015-10-08 11:41:49 +0200

    formatted all

---
+++ src/test/GreedySpannerTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import junit.framework.Test; 
+import junit.framework.TestSuite; 
+import junit.framework.TestCase;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/** 
+* GreedySpanner Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>10/08/2015</pre> 
+* @version 1.0 
+*/ 
+public class GreedySpannerTest extends TestCase { 
+public GreedySpannerTest(String name) { 
+super(name); 
+} 
+
+public void setUp() throws Exception { 
+super.setUp(); 
+} 
+
+public void tearDown() throws Exception { 
+super.tearDown(); 
+} 
+
+/** 
+* 
+* Method: GreedySpanner(ListenableUndirectedWeightedGraph g, Integer r) 
+* 
+*/ 
+public void testGreedySpanner() throws Exception { 
+
+    assertEquals();
+
+}
+
+/** 
+* 
+* Method: sortHashMapByValuesD(HashMap passedMap) 
+* 
+*/ 
+public void testSortHashMapByValuesD() throws Exception {
+
+    LinkedHashMap sorted = new
+}
+
+
+
+public static Test suite() { 
+return new TestSuite(GreedySpannerTest.class); 
+} 
+} 
