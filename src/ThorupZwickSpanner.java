0c168ca8ebc749f47c8117760c2e28397281b034
bathemean <blackapple.chr@gmail.com>
2015-10-08 18:17:15 +0200

    TREES

--- src/ThorupZwickSpanner.java
+++ src/ThorupZwickSpanner.java
@@ -1,25 +1,28 @@
 
+import org.jgrapht.alg.DijkstraShortestPath;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ThorupZwickSpanner {
 
-
+    int k;
 
     public ThorupZwickSpanner(uwGraph g, int k) {
+
+        this.k = k;
 
         ArrayList<String> vertices = vertexSetToArray(g.vertexSet());
 
         // Assign vertices into parititions.
-        HashMap<Integer, ArrayList<String>> p = nonRandPartition(vertices, k);
-
-        System.out.println(p.toString());
+        HashMap<Integer, ArrayList<String>> p = nonRandPartition(vertices);
 
         // Compute distance between A_k and every vertex v.
         //distances(partition p, vertex v);
 
-        // Grow shortest path tree from some w in A_i - A_i+1.
-        //tree(w, vertices);
-
+        ArrayList<ArrayList<DijkstraShortestPath>> spt = shortestPathsTrees(g, p);
+        System.out.println(spt);
+        
         // UNITE THE TREES!
         //uniion(trees);
 
@@ -39,8 +42,8 @@ public class ThorupZwickSpanner {
         return array;
 
     }
-    
-    private HashMap<Integer, ArrayList<String>> nonRandPartition(ArrayList<String> vertices, int k) {
+
+    private HashMap<Integer, ArrayList<String>> nonRandPartition(ArrayList<String> vertices) {
 
         HashMap<Integer, ArrayList<String>> partitions = new HashMap<>();
 
@@ -64,7 +67,7 @@ public class ThorupZwickSpanner {
 
     }
 
-    private HashMap<Integer, ArrayList<String>> partition(ArrayList<String> vertices, int k) {
+    private HashMap<Integer, ArrayList<String>> partition(ArrayList<String> vertices) {
 
         HashMap<Integer, ArrayList<String>> partitions = new HashMap<>();
 
@@ -97,6 +100,51 @@ public class ThorupZwickSpanner {
 
         return partitions;
 
+    }
+
+    private HashMap<HashMap<ArrayList<String>, Object>, Integer> distances() {
+        return null;
+    }
+
+    private ArrayList<ArrayList<DijkstraShortestPath>> shortestPathsTrees(uwGraph g, HashMap<Integer, ArrayList<String>> partitions) {
+
+        ArrayList<ArrayList<DijkstraShortestPath>> shortestPaths = new ArrayList<>();
+
+        for(int i = 0; i < k; i++) {
+            ArrayList<String> subset = new ArrayList<>();
+
+            ArrayList<String> cur = partitions.get(i);
+            ArrayList<String> next = partitions.get(i+1);
+
+            for(String c : cur) {
+
+                if(next != null) {
+                    if (!next.contains(c)) {
+                        subset.add(c);
+                    }
+                } else {
+                    subset.add(c);
+                }
+
+            }
+
+
+            if(next != null) {
+                for (String w : subset) {
+                    ArrayList<DijkstraShortestPath> sp = new ArrayList<>();
+
+                    for (String v : next) {
+                        DijkstraShortestPath p = new DijkstraShortestPath(g, w, v);
+                        sp.add(p);
+                    }
+                    shortestPaths.add(sp);
+                }
+            }
+
+        }
+
+
+        return shortestPaths;
     }
 
 }
