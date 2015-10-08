d5e735b20498ed2fa2a77054185523d9a73bf50b
bathemean <blackapple.chr@gmail.com>
2015-10-08 11:53:54 +0200

    VEJKORT TIL TZ

--- src/ThorupZwickSpanner.java
+++ src/ThorupZwickSpanner.java
@@ -3,6 +3,23 @@
  */
 public class ThorupZwickSpanner {
 
-    
+    public ThorupZwickSpanner(uwGraph stringGraph, int k) {
+
+        // Assign vertices into parititions.
+        partition(vertices, k);
+
+        // Compute distance between A_k and every vertex v.
+        distances(partition p, vertex v);
+
+        // Grow shortest path tree from some w in A_i - A_i+1.
+        tree(w, vertices);
+
+        // UNITE THE TREES!
+        uniion(trees);
+
+        
+
+
+    }
 
 }
