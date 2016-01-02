23873692dc5900771592d04869adb9b2f63a25da
Allan Nielsen <an@quantads.net>
2016-01-02 16:30:34 +0100

    No time to commit.. My people need me!

--- src/main/Main.java
+++ src/main/Main.java
@@ -33,26 +33,31 @@ public class Main {
         // for k = 2, 3, 5.
         for(int k = 1; k < 10; k++) {
             // for vertice counts {25, 50, ..., 400}.
+            System.out.println(k);
             for (int v = 25; v < 400; v += 25) {
                 // for densities d = {0.5, 0.6, ..., 1.0}.
                 for (double d = 0.8; d <= 1.0; d += 0.1) {
-                    String filename = "density" + d + "_vertices" + v + "_k" + k + ".txt";
-                    PrintWriter writerGreedy = new PrintWriter("Greedy_" + filename, "UTF-8");
-                    PrintWriter writerTZ = new PrintWriter("TZ_" + filename, "UTF-8");
+                    String descriptionHeaders = "weight,density,highest degree,runtime\n";
+                    String filename = "density" + d + "_vertices" + v + "_k" + k + ".csv";
+
+                    PrintWriter writerGreedy = new PrintWriter("/datapoints/Greedy_" + filename, "UTF-8");
+                    PrintWriter writerTZ = new PrintWriter("/datapoints/TZ_" + filename, "UTF-8");
+
+                    writerGreedy.print(descriptionHeaders);
+                    writerTZ.print(descriptionHeaders);
+
+
+                    uwGraph generatedGraph = factory.wieghtedDenseGraph(v, d);
+
+                    GreedySpanner greedy = new GreedySpanner();
+                    uwGraph greedyspan = greedy.makeSpanner(generatedGraph, 2 * k - 1);
+                    writerGreedy.print(greedyspan.getMetricsAsCSV());
 
                     // We want to get a few datapoints to even out any factors
                     for (int i = 0; i < 10; i++) {
-                        GreedySpanner greedy = new GreedySpanner();
                         ThorupZwickSpanner thorupzwick = new ThorupZwickSpanner();
-
-                        uwGraph generatedGraph = factory.wieghtedDenseGraph(v, d);
-
-                        uwGraph greedyspan = greedy.makeSpanner(generatedGraph, 2 * k - 1);
-                        writerGreedy.print(greedyspan.getMetricsAsString());
-
                         uwGraph tzspan = thorupzwick.makeSpanner(generatedGraph, k);
-                        writerTZ.print(tzspan.getMetricsAsString());
-
+                        writerTZ.print(tzspan.getMetricsAsCSV());
                     }
                     writerGreedy.close();
                     writerTZ.close();
